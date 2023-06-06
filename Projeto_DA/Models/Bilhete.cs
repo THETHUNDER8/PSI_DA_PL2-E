5b46965443e50bb088a7aafca383c480ee0a33bd
Claudio <100351232+ClaudioAlex186@users.noreply.github.com>
2023-06-06 17:52:54 +0100

    itextsharp

--- Projeto_DA/Models/Bilhete.cs
+++ Projeto_DA/Models/Bilhete.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing.Printing;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
-using iTextSharp.text.pdf;//bibliotecas para dar export em pdf,txt etc...
 using iTextSharp.text;
-using System.Windows.Documents;
-using Org.BouncyCastle.Crypto;
+using iTextSharp.text.pdf;//bibliotecas para dar export em pdf,txt etc...
+using System.IO;
+
 
 namespace Projeto_DA.Models
 {
@@ -27,35 +23,34 @@ namespace Projeto_DA.Models
             lugar = Lugar;
             estado = Estado;
         }
-    }
+
+        public void exportar()
+        {
+
+            var date = DateTime.Now;
+            string date_str = "Data: " + date.ToString();
+            string filmevisto = "Filme: ";
+            Image imagem = Image.GetInstance("img/img.jpg");//diretório da imagem para o ticket
+            imagem.SetAbsolutePosition(10, 10);
+
+            var doc = new Document(PageSize.A6);//Tamanho da página do ticket, escolhi A6 porque é um tamanho mais pequeno
+            PdfWriter.GetInstance(doc, new FileStream("D:/ticket.pdf", FileMode.Create));
+
+            doc.Open();
+
+            Console.WriteLine(date_str);
+            Paragraph titulo = new Paragraph("Bem vindo ao cinema!", new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD));
+            titulo.Alignment = Element.ALIGN_CENTER;
+
+            doc.Add(titulo);
+            doc.Add(new Paragraph(date_str));
+            doc.Add(new Paragraph(filmevisto));
+
+            doc.Add(imagem);
 
 
+            doc.Close();
+        }
 
-    public void exportar()
-    {
-
-        var date = DateTime.Now;
-        string date_str = "Data: " + date.ToString();
-        string filmevisto = "Filme: ";
-        Image imagem = Image.GetInstance("img/img.jpg");//diretório da imagem para o ticket
-        imagem.SetAbsolutePosition(10, 10);
-
-        var doc = new Document(PageSize.A6);//Tamanho da página do ticket, escolhi A6 porque é um tamanho mais pequeno
-        PdfWriter.GetInstance(doc, new FileStream("D:/ticket.pdf", FileMode.Create));
-
-        doc.Open();
-
-        Console.WriteLine(date_str);
-        Paragraph titulo = new Paragraph("Bem vindo ao cinema!", new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD));
-        titulo.Alignment = Element.ALIGN_CENTER;
-
-        doc.Add(titulo);
-        doc.Add(new Paragraph(date_str));
-        doc.Add(new Paragraph(filmevisto));
-
-        doc.Add(imagem);
-
-
-        doc.Close();
     }
 }
